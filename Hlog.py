f863d29e75adf92472a0791460bb18f80a970c79
Hanyuu Furude <HanyuuFurude@outlook.com>
2019-03-04 09:46:19 +0800

    201903040946

---
+++ Hlog.py
@@ -0,0 +1,32 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s %(filename)s]\n  line:%(lineno)d,level:%(levelname)s,message:%(message)s\n',
+    datefmt='%Y/%b/%d %H:%M:%S',
+    filename='log.log',
+    filemode='a'
+)
+
+# debug() 调试级别，一般用于记录程序运行的详细信息
+debug = 0
+# info() 事件级别，一般用于记录程序的运行过程
+info = 1
+# warning() 警告级别，，一般用于记录程序出现潜在错误的情形
+warning = 2
+# error() 错误级别，一般用于记录程序出现错误，但不影响整体运行
+error = 3 
+# critical 严重错误级别 ， 出现该错误已经影响到整体运行
+critical = 4
+
+logList = {
+    debug:logging.debug,
+    info:logging.info,
+    warning:logging.warning,
+    critical:logging.critical
+    }
+def Hlog(array:list,mode:int = 0,onScreen:bool = False)->None:
+    logText = ''
+    for x in array:
+        logText += '\n[ %s ] %s' %(x,array[x])
+    logList[mode](logText)
--- logout.py
+++ logout.py
@@ -3,6 +3,16 @@ import re
 import base64
 import os
 import sys
+import json
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s %(filename)s]\n  line:%(lineno)d,level:%(levelname)s,message:%(message)s\n',
+    datefmt='%Y/%b/%d %H:%M:%S',
+    filename='log.log',
+    filemode='a'
+)
 headers = \
     {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) \
@@ -14,3 +24,5 @@ if __name__ == '__main__':
     print(arg)
     res = requests.post('https://w.seu.edu.cn/index.php/index/logout', headers=headers,verify=False)
     print(res.text)
+    resTest = json.loads(res.text)
+    logText = ''
